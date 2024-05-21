c73e52fb39b030ce1cf621d5b42ac2ea364ce92b
rifaa oumnia <rifaaoumnia@gmail.com>
2024-05-21 21:48:23 +0100

    Initial commit

---
+++ eBanking-backend/src/main/java/ma/enset/ebankingbackend/mappers/BankAccountMapperImpl.java
@@ -0,0 +1,25 @@
+package ma.enset.ebankingbackend.mappers;
+
+import ma.enset.ebankingbackend.dtos.CustomerDTO;
+import ma.enset.ebankingbackend.entities.Customer;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+//MapStruct
+@Service
+public class BankAccountMapperImpl {
+    public CustomerDTO fromCustomer(Customer customer){
+        CustomerDTO customerDTO= new CustomerDTO();
+        BeanUtils.copyProperties(customer,customerDTO);
+        //customerDTO.setId(customer.getId());
+        // customerDTO.setName(customer.getName());
+        //customerDTO.setEmail(customer.getEmail());
+        return customerDTO;
+    }
+
+    public Customer fromCustomerDTO(CustomerDTO customerDTO){
+        Customer customer=new Customer();
+        BeanUtils.copyProperties(customerDTO,customer);
+        return customer;
+    }
+}
